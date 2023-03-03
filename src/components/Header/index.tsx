2585eaf7c208239432e01e54f1608a040d97098d
Gustavo Santos <devgustavosantos@gmail.com>
2023-03-03 06:41:07 -0300

    fix: fix style of 'Header' component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,11 @@ import { Wrapper } from '../Wrapper';
 import { HeaderBottom } from './components/HeaderBottom';
 import { HeaderMid } from './components/HeaderMid';
 import { HeaderTop } from './components/HeaderTop';
+import styles from './styles.module.scss';
 
 export function Header() {
   return (
-    <header>
+    <header className={styles.container}>
       <Wrapper>
         <HeaderTop />
         <HeaderMid />
