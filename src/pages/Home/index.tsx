9eda274d31349c0f88822dda722bc7edc88ac200
Gustavo Santos <devgustavosantos@gmail.com>
2023-03-03 06:55:30 -0300

    feat: creates the 'Banner' component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
+import { Banner } from '../../components/Banner';
 import { Header } from '../../components/Header';
 
 export function Home() {
   return (
     <>
-      {' '}
-      <Header />{' '}
+      <Header />
+      <Banner />
     </>
   );
 }
