92d83bdfd0750f15cc12483e4dac451ea302e957
Gustavo Santos <devgustavosantos@gmail.com>
2023-03-04 08:32:09 -0300

    feat: creates the 'Carrousel' component

---
+++ src/components/Carrousel/index.tsx
@@ -0,0 +1,50 @@
+import arrow from '../../assets/arrow.svg';
+import { ProductInfos } from '../../types/product';
+import { Card } from '../Card';
+import { Wrapper } from '../Wrapper';
+import styles from './styles.module.scss';
+
+type CarrouselProps = {
+  products: ProductInfos[];
+};
+
+const arrowDescription = 'Desenho de uma flecha apontada para ';
+
+export function Carrousel({ products }: CarrouselProps) {
+  return (
+    <Wrapper>
+      <div className={styles.container}>
+        <button
+          className={styles.buttons}
+          title={`${arrowDescription} esquerda.`}
+        >
+          <img
+            src={arrow}
+            alt={`${arrowDescription} esquerda.`}
+          />
+        </button>
+
+        <div className={styles.slider}>
+          {products.map((product) => (
+            <Card
+              key={product.productName}
+              productName={product.productName}
+              photo={product.photo}
+              price={product.price}
+            />
+          ))}
+        </div>
+
+        <button
+          className={styles.buttons}
+          title={`${arrowDescription} direita.`}
+        >
+          <img
+            src={arrow}
+            alt={`${arrowDescription} direita.`}
+          />
+        </button>
+      </div>
+    </Wrapper>
+  );
+}
