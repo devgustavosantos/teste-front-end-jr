1a1baf0bf924d58543362679b77c9d235d76e5b9
Gustavo Santos <devgustavosantos@gmail.com>
2023-03-05 11:32:35 -0300

    refactor: simplifies the structure of the 'Carrousel' component

--- src/components/Carrousel/index.tsx
+++ src/components/Carrousel/index.tsx
@@ -1,7 +1,6 @@
 import arrow from '../../assets/arrow.svg';
 import { ProductInfos } from '../../types/product';
 import { Card } from '../Card';
-import { Wrapper } from '../Wrapper';
 import styles from './styles.module.scss';
 
 type CarrouselProps = {
@@ -12,40 +11,38 @@ const arrowDescription = 'Desenho de uma flecha apontada para ';
 
 export function Carrousel({ products }: CarrouselProps) {
   return (
-    <Wrapper>
-      <div className={styles.container}>
-        <button
-          className={styles.buttons}
-          title={`${arrowDescription} esquerda.`}
-        >
-          <img
-            src={arrow}
-            alt={`${arrowDescription} esquerda.`}
-          />
-        </button>
+    <div className={styles.container}>
+      <button
+        className={styles.buttons}
+        title={`${arrowDescription} esquerda.`}
+      >
+        <img
+          src={arrow}
+          alt={`${arrowDescription} esquerda.`}
+        />
+      </button>
 
-        <div className={styles.slider}>
-          {products.map((product, index) => (
-            <Card
-              //ALTERAR A KEY PARA NÃO USAR O INDEX DO ARRAY!!!
-              key={index}
-              productName={product.productName}
-              photo={product.photo}
-              price={product.price}
-            />
-          ))}
-        </div>
-
-        <button
-          className={styles.buttons}
-          title={`${arrowDescription} direita.`}
-        >
-          <img
-            src={arrow}
-            alt={`${arrowDescription} direita.`}
+      <div className={styles.slider}>
+        {products.map((product, index) => (
+          <Card
+            //ALTERAR A KEY PARA NÃO USAR O INDEX DO ARRAY!!!
+            key={index}
+            productName={product.productName}
+            photo={product.photo}
+            price={product.price}
           />
-        </button>
+        ))}
       </div>
-    </Wrapper>
+
+      <button
+        className={styles.buttons}
+        title={`${arrowDescription} direita.`}
+      >
+        <img
+          src={arrow}
+          alt={`${arrowDescription} direita.`}
+        />
+      </button>
+    </div>
   );
 }
