440906f916557ec548bfe9141132ae921f0d4717
Gustavo Santos <devgustavosantos@gmail.com>
2023-03-05 00:14:35 -0300

    feat: improves the functionality of the 'Wrapper' component

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,11 +1,18 @@
 import { ReactNode } from 'react';
 
 import styles from './styles.module.scss';
+import { useWrapper } from './useWrapper';
 
 type WrapperProps = {
   children: ReactNode;
+  secondClassName?: string;
 };
 
-export function Wrapper({ children }: WrapperProps) {
-  return <div className={styles.container}>{children}</div>;
+export function Wrapper({ children, secondClassName }: WrapperProps) {
+  const { finalClassName } = useWrapper({
+    firstClassName: styles.container,
+    secondClassName,
+  });
+
+  return <div className={finalClassName}>{children}</div>;
 }
