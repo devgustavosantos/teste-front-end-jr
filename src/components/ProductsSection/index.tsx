bc0226cd58ee6b9bdc8cba01934121a2c4377ed2
Gustavo Santos <devgustavosantos@gmail.com>
2023-03-05 15:19:02 -0300

    refactor: simplifies the structure of the 'ProductsSection' component

---
+++ src/components/ProductsSection/index.tsx
@@ -0,0 +1,39 @@
+import { Card } from '../Card';
+import { Carrousel } from '../Carrousel';
+import { Subcategories } from '../Subcategories';
+import { Wrapper } from '../Wrapper';
+import { products } from './data';
+import styles from './styles.module.scss';
+
+type SectionTitleProps = {
+  withSubcategories: boolean;
+};
+
+export function ProductsSection({ withSubcategories }: SectionTitleProps) {
+  return (
+    <Wrapper>
+      <h2 className={styles.title}>Produtos relacionados</h2>
+      {withSubcategories && <Subcategories />}
+      {!withSubcategories && (
+        <a
+          href="#"
+          className={styles.link}
+        >
+          Ver todos
+        </a>
+      )}
+
+      <Carrousel variant="products">
+        {products.map((product, index) => (
+          <Card
+            //ALTERAR A KEY PARA NÃO USAR O INDEX DO ARRAY!!!
+            key={index}
+            productName={product.productName}
+            photo={product.photo}
+            price={product.price}
+          />
+        ))}
+      </Carrousel>
+    </Wrapper>
+  );
+}
