cd23b41d1acbdae0ebc3c359d6afffc4ea2e4399
Gustavo Santos <devgustavosantos@gmail.com>
2023-03-05 19:05:20 -0300

    feat: creates the structure and styling of the 'Home' page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,55 @@
 import { Banner } from '../../components/Banner';
+import { Brands } from '../../components/Brands';
 import { Categories } from '../../components/Categories';
+import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
+import { Poster } from '../../components/Poster';
+import { ProductsSection } from '../../components/ProductsSection';
+import { Wrapper } from '../../components/Wrapper';
+import styles from './styles.module.scss';
 
 export function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Header />
+
       <Banner />
+
       <Categories />
-    </>
+
+      <ProductsSection withSubcategories />
+
+      <Wrapper secondClassName={styles.wrapperHome}>
+        <Poster
+          title="Parceiros"
+          content="Lorem ipsum dolor sit amet, consectetur"
+          variant="large"
+        />
+        <Poster
+          title="Parceiros"
+          content="Lorem ipsum dolor sit amet, consectetur"
+          variant="large"
+        />
+      </Wrapper>
+
+      <ProductsSection withSubcategories={false} />
+
+      <Wrapper secondClassName={styles.wrapperHome}>
+        <Poster
+          title="Produtos"
+          content="Lorem ipsum dolor sit amet, consectetur"
+          variant="middle"
+        />
+        <Poster
+          title="Produtos"
+          content="Lorem ipsum dolor sit amet, consectetur"
+          variant="middle"
+        />
+      </Wrapper>
+
+      <Brands />
+
+      <Footer />
+    </div>
   );
 }
